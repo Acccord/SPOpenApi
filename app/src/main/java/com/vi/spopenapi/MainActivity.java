37c34aef59c9e5440fb21aa0559a189391b4d6b0
vi <380180278@qq.com>
2019-11-25 19:38:25 +0800

    frist

---
+++ app/src/main/java/com/vi/spopenapi/MainActivity.java
@@ -0,0 +1,18 @@
+package com.vi.spopenapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.vi.openapi.TempSerial;
+import com.vi.vioserial.VioSerial;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
